05809f24619ba177498454446d395c14f90f0d9e
ZenekGit <149100942+ZenekGit@users.noreply.github.com>
2024-11-24 14:25:26 +0100

    add  UserController and routs

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,21 +3,71 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Client;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $Client = Client::all();
-
-        return  response()->json($Client,200);
+        $users = User::all();
+        return response()->json($users);
     }
-
     public function show($id)
     {
-      
-       return Client::find($id);
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        return response()->json($user);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,' . $id,
+            'password' => 'nullable|string',
+            'logo' => 'nullable|string',
+        ]);
+
+        try {
+            $user = User::findOrFail($id);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        $user->name = $validatedData['name'];
+        $user->email = $validatedData['email'];
+
+        if ($validatedData['password']) {
+            $user->password = bcrypt($validatedData['password']);
+        }
+
+        $user->logo = $validatedData['logo'] ?? null;
+
+        try {
+            $user->save();
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Failed to update user'], 500);
+        }
+
+        return response()->json($user);
+    }
+
+
+    public function destroy($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted'], 204);
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\EstimationController;
+use App\Http\Controllers\UserController;
 
 
 Route::post('/register',[AuthenticationController::class, 'register']);
@@ -24,12 +25,19 @@ Route::post('/projects', [ProjectController::class, 'store']);
 Route::put('/projects/{id}', [ProjectController::class, 'update']);
 Route::delete('/projects/{id}', [ProjectController::class, 'delete']);
 
-
 Route::get('/estimations', [EstimationController::class, 'index']);
 Route::get('/estimations/{id}', [EstimationController::class, 'show']);
 Route::post('/estimations', [EstimationController::class, 'store']);
 Route::put('/estimations/{id}', [EstimationController::class, 'update']);
 Route::delete('/estimations/{id}', [EstimationController::class, 'delete']);
+
+Route::get('/users', [UserController::class,'index']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::put('/users', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
