1ebaeb540fab6712d0788ca0e50457ffa64f5e47
G4brielllo <gabrieltarasiuk3@gmail.com>
2024-12-10 22:25:52 +0100

    bug fix

--- database/migrations/2024_11_19_200339_create_users_table.php
+++ database/migrations/2024_11_19_200339_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string ('name');
             $table->string ('email')->uniqe();
             $table->string ('password');
-            $table->enum('role', ['User', 'admin'])->default('user');
+            $table->enum('role', ['User', 'Admin'])->default('User');
             $table->longText('logo')->nullable();
             $table->timestamps();
         });
