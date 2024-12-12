2519a0c1e5c7e8248e84c332eb2dc92ea2f3867c
ZenekGit <149100942+ZenekGit@users.noreply.github.com>
2024-12-12 14:28:45 +0100

    repair web.php

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::post('/logout',[AuthenticationController::class, 'logout']);
 
 
 Route::get('/users', [UserController::class,'index']);
+Route::get('users/{id}', [UserController::class, 'show']);
 Route::put('/users/{id}', [UserController::class, 'update']);
 Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
